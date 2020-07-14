553c65ce5e73092d4b144e49860ca6da708b30a8
mukesh <mukesh@jogiinfotech.com>
2020-07-14 09:44:45 +0530

    Tuple and Set examples.

---
+++ Unit 1/Collections/Set/set1.py
@@ -0,0 +1,9 @@
+# Declaring Set
+set1 = {"Apple","Banana","Cherry","Mango","Pinapple"}
+print(set1)
+
+# Iterate through Set
+set1 = {"Apple","Banana","Cherry","Mango","Pinapple"}
+
+for item in set1:
+    print(item)
--- Unit 1/Collections/Tuple/tuple1.py
+++ Unit 1/Collections/Tuple/tuple1.py
@@ -24,3 +24,25 @@ print(fruit_tuple)
 # Delete entire tuple
 fruit_tuple = ("Apple","Banana","Mango")
 del fruit_tuple
+
+# Declaring tuple with single item. Comma is required after first tiem.
+mytuple = ("apple",)
+print(type(mytuple))
+
+# Joining tuples
+tuple1 = ("a","b","c")
+tuple2 = (1,2,3,4)
+tuple3 = tuple1 + tuple2
+print(tuple3)
+
+# Create tuple using constructor
+tuple1 = tuple(("a","b","c"))
+print(tuple1)
+
+# Count Method
+tuple1 = tuple(("a","b","c","a","b","a"))
+print(tuple1.count("c"))
+
+# Index Method
+tuple1 = tuple(("a","b","c","a","b","a"))
+print(tuple1.index("c"))
