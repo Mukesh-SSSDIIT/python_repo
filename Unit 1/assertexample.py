ef3afa65e2e9bcaecdb59059e8e34cc8f2cca695
mukesh <mukesh@jogiinfotech.com>
2020-07-23 09:50:31 +0530

    Module, Assert and File IO Examples

---
+++ Unit 1/assertexample.py
@@ -0,0 +1,10 @@
+a = 20
+
+assert a <= 10
+
+print("Program ends")
+
+x = "hello"
+
+#if condition returns False, AssertionError is raised:
+assert x == "goodbye", "x should be 'hello'"
---
+++ Unit 1/file_io/fileread.py
@@ -0,0 +1,4 @@
+fp = open("E:\\Python Programs\\python_repo\\Unit 1\\file_io\\myfile.txt")
+
+txt = fp.read()
+print(txt)
