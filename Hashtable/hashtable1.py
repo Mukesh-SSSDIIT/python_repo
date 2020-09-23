81302b283fe1c5e982a8f67a963e3f707315622b
mukesh <mukesh@jogiinfotech.com>
2020-09-23 09:50:23 +0530

    Hashtable and RegExp Introduction

---
+++ Hashtable/hashtable1.py
@@ -0,0 +1,17 @@
+student = {
+    "name":"Rakesh",
+    "class":"bca",
+    "age":"19"
+    }
+print(hash(student["name"]))
+print(hash(student["class"]))
+print(hash(student["age"]))
+
+a = "India"
+print(hash(a))
+
+b = "India"
+print(hash(b))
+
+c = "India"
+print(hash(c))
---
+++ RegularExpression/re1.py
@@ -0,0 +1,3 @@
+import re
+m = re.search("How","Hi foo, How are you");
+print(m)
