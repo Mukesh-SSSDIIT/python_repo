46ca3baa8c1dbead18e204474658e78e57ff61f9
mukesh <mukesh@jogiinfotech.com>
2020-09-28 09:45:16 +0530

    RegEx

--- RegularExpression/re-findall.py
+++ RegularExpression/re-findall.py
@@ -63,3 +63,75 @@ print(x)
 txt = "The rain you in Sp_ain @ 09:30am,# ^ how are you"
 x = re.findall("[0-9]", txt)
 print(x)
+
+txt = "The rain you in Sp_ai16n @ 09:30am,# ^ how are you"
+x = re.findall("[0-5][0-9]", txt)
+print(x)
+
+txt = "The rain you in Sp_ai16n @ 09:30am,# ^ how are you"
+x = re.findall("[a-zA-Z0-9]", txt)
+print(x)
+
+txt = "The rain you in Sp_ai16n @ 093:30am,# ^ how are you"
+x = re.findall("[0-9]...", txt)
+print(x)
+
+txt = "The rain you in Sp_ai16n @ 093:30am,# ^ how are you"
+x = re.findall("^The rain", txt)
+print(x)
+
+txt = "The rain you in Sp_ai16n @ 093:30am,# ^ how are you"
+x = re.findall("are you$", txt)
+print(x)
+
+txt = "The rain you in Sp_ai16n @ 093:30am,# ^ how are you"
+x = re.findall("[0-9]*", txt)
+print(x)
+
+txt = "The rain you in Sp_ai16n @ 093:30am,# ^ how74a5 are you"
+x = re.findall("[0-9]+", txt)
+print(x)
+
+txt = "The rain you in Sp_ai16n @ 093:30am,# ^ how7457 are you"
+x = re.findall("[0-9]{2}", txt)
+print(x)
+
+txt = "We are Indian, we live in hindustan, bharat"
+x = re.findall("Indian|hindustan|bharat", txt)
+print(x)
+
+txt = "We are Indian, we live in hindustan, bharat"
+x = re.search("Indian|hindustan|bharat", txt)
+print(x.start())
+print(x.end())
+print(x.span())
+print(x.string)
+print(x.group())
+
+txt = "We are India, we live in hind, bharat"
+x = re.search("Indian|hindustan|bharat", txt)
+if x == None:
+    print("Search not found")
+else:
+    print(x.start())
+    print(x.end())
+    print(x.span())
+    print(x.string)
+    print(x.group())
+
+
+txt = "We are Indian, we live in hind, bharat"
+x = re.split("\s", txt)
+print(x)
+for i in x:
+    print(i)
+
+txt = "We are Indian, we live in hind, bharat"
+x = re.split("\s", txt,3)
+print(x)
+for i in x:
+    print(i)
+
+txt = "I love my India, India is my country, You also love India"
+x = re.sub("India","Bharat", txt,2)
+print(x)
