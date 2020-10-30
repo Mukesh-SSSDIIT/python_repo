0722e8ca48fcee2ac538a676acd2c55ce22120dd
mukesh <mukesh@jogiinfotech.com>
2020-10-30 09:35:55 +0530

    Misc changes

--- pythonmysql/tclcommand-example.py
+++ pythonmysql/tclcommand-example.py
@@ -11,12 +11,15 @@ mydb.autocommit =False
 
 mycursor = mydb.cursor()
 
-args = ('aaa',1)
+args = ('bbb',1)
 mycursor.execute("update student set student_name = %s where student_id = %s",args)
 
-args = ('aaaaaaa',2)
+args = ('bbbbbb',2)
 mycursor.execute("update student set student_name = %s where student_id = %s",args)
 
 mydb.rollback()
 
 print(mycursor.rowcount, "record(s) updated successfully")
+
+mycursor.close()
+mydb.close()
