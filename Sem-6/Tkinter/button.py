d34886676b5caab048f644729c94bd0bc0e6e269
mukesh <mukesh.jogiinfotech@gmail.com>
2020-12-26 09:18:08 +0530

    Basic changes

--- Sem-6/Tkinter/button.py
+++ Sem-6/Tkinter/button.py
@@ -5,7 +5,11 @@ from tkinter import Button
 def btn_click():
     msg = messagebox.showinfo( "Hello Python", "Welcome to python")
 
-top = tk.Tk()
-btn = Button(top,text="Click Me !!",command=btn_click)
-btn.place(x= 100,y=100)
-top.mainloop()
+topwindow = tk.Tk(screenName='Test', className='My First Program')
+btn1 = Button(topwindow,text="Click Me !! - I am button 1",command=btn_click)
+btn2 = Button(topwindow,text="Click Me !! - I am button 2",command=btn_click)
+btn3 = Button(topwindow,text="Click Me !! - I am button 3",command=btn_click)
+btn1.place(x= 100,y=50)
+btn2.place(x= 100,y=100)
+btn3.place(x= 100,y=150)
+topwindow.mainloop()
