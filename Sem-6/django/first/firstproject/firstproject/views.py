221a090edab960687058faacb6ad33b4b5ee2991
Mukesh <mukesh.jogiinfotech@gmail.com>
2021-02-21 20:39:04 +0530

    django Example

---
+++ Sem-6/django/first/firstproject/firstproject/views.py
@@ -0,0 +1,5 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+def home(request):
+    return HttpResponse("<h1>Hello World</h1>, Jay Swaminarayan")
