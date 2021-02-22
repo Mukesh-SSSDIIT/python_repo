6097a05b9450e955bcc16740c22860445ba4d5b5
Mukesh <mukesh.jogiinfotech@gmail.com>
2021-02-22 09:18:16 +0530

    Matplotlib Example

--- Sem-6/Plotting/first.py
+++ Sem-6/Plotting/first.py
@@ -1,5 +1,13 @@
-from matplotlib import pyplot as plt  
-#ploting our canvas  
-plt.plot([1,2,3],[4,5,1])  
-#display the graph  
-plt.show()  
+from matplotlib import pyplot as plt 
+import matplotlib 
+# print(matplotlib.__version__)  
+
+x = [1,2,3,4]
+y = [4,15,20,21]
+plt.plot(x,y,"ro")  
+
+plt.title('Today\'s Cricket Match')  
+plt.ylabel('Runs')  
+plt.xlabel('Overs')  
+
+plt.show()
