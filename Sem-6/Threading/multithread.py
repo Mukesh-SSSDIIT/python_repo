38a52c64541ef2e453718eb0108d8da5ea0ccc08
Mukesh <mukesh.jogiinfotech@gmail.com>
2021-03-02 09:04:06 +0530

    Multithreading Examples

--- Sem-6/Threading/multithread.py
+++ Sem-6/Threading/multithread.py
@@ -1,16 +1,21 @@
-from threading import *
+import threading
 from time import sleep
-class C1(Thread):
-    def run(self):
+class C1(threading.Thread):
+    def __init__(self,a,b): # Overriding Thread class(baseclass) __init__ method
+        threading.Thread.__init__(self)
+        self.a = a
+        self.b = b
+    def run(self): # Overriding Thread class(baseclass) run method
         for i in range(5):
-            print("Hello")
+            print("Sum",self.a+self.b)
             sleep(1)
-class C2(Thread):
+
+class C2(threading.Thread):
     def run(self):
         for i in range(5):
             print("Hi")
-            # sleep(1)
-obj1 = C1()
+            sleep(1)
+obj1 = C1(5,6)
 obj2 = C2()
 obj1.start()
 obj2.start()
