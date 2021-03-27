7e12cfba65d46d7989bd76426c9277278b9548c5
Mukesh Jogi <mukesh@jogiinfotech.com>
2021-03-27 08:54:51 +0530

    CRUD Example.

---
+++ Sem-6/django/crudexample/employee/forms.py
@@ -0,0 +1,7 @@
+from django import forms  
+from employee.models import Employee
+class EmployeeForm(forms.ModelForm):  
+    class Meta:  
+        model = Employee  
+        fields = "__all__"  
+        #fields = ['eid','ename','ecity']
